475c9fc0d568ee3a2f33fa7074f4284453635e90
chaos <chaos@CustoPro.local>
2017-07-18 18:53:04 +0200

    FeatureTracking - Lets see if that works :D

--- lib/server/main.lua
+++ lib/server/main.lua
@@ -1,1 +1,23 @@
 print("We got somewhere with the server")
+
+if ChaosCollection then
+  if ChaosCollection.Features then
+    print("we have features")
+    if ChaosCollection.Features.Essentials
+      print("we have essentials")
+    else
+      print("we DONT have essentials")
+      ChaosCollection.Features.Essentials = true
+      if ChaosCollection.Features.Essentials
+        print("we have essentials")
+      else
+        print("we DONT have essentials")
+        ChaosCollection.Features.Essentials = true
+      end
+    end
+  else
+    print("we DONT have features")
+  end
+else
+  print("we DONT have a collection")
+end
