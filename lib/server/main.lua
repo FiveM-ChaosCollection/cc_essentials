193b7333853b3d06e10d995d3620c658a488414a
chaos <chaos@CustoPro.local>
2017-07-18 18:56:12 +0200

    I'm going to love this... NOT

--- lib/server/main.lua
+++ lib/server/main.lua
@@ -3,12 +3,12 @@ print("We got somewhere with the server")
 if ChaosCollection then
   if ChaosCollection.Features then
     print("we have features")
-    if ChaosCollection.Features.Essentials
+    if ChaosCollection.Features.Essentials then
       print("we have essentials")
     else
       print("we DONT have essentials")
       ChaosCollection.Features.Essentials = true
-      if ChaosCollection.Features.Essentials
+      if ChaosCollection.Features.Essentials then
         print("we have essentials")
       else
         print("we DONT have essentials")
