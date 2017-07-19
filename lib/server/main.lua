f53f13d60c8b8ebeefe06fbd4cb73789a930f256
chaos <chaos@CustoPro.local>
2017-07-19 15:23:44 +0200

    Fuu

--- lib/server/main.lua
+++ lib/server/main.lua
@@ -1,3 +1,16 @@
 print("We got somewhere with the server")
 createPprint().pprint(exports)
 createPprint().pprint(exports.ChaosCollection)
+
+
+-- register feature
+if exports.ChaosCollection && exports.ChaosCollection.AvailableFeatures then
+  if exports.ChaosCollection.AvailableFeatures.Essentials == nil then
+    exports.ChaosCollection.AvailableFeatures.Essentials = true
+    print("[CC-Essentials-LOG] Registered feature `Essentials' with ChaosCollection.AvailableFeatures")
+  else
+    print("[CC-Essentials-ERROR] Failed to register feature `Essentials' with ChaosCollection.AvailableFeatures: Already registered!")
+  end
+else
+  print("[CC-Essentials-ERROR] Failed to register feature `Essentials' with ChaosCollection.AvailableFeatures: No such variable!")
+end
